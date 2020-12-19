723116e9e3ccf3cdbb6c676db77d995983734631
Jerrod Styrk <jstyrk@citadel.edu>
2020-12-18 16:35:09 -0800

    Fail Vendor Constructor test

---
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class VendorTest
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor ();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
