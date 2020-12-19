6af1723004cf1cdc49e50ba31bfe62abfe1a5bae
Jerrod Styrk <jstyrk@citadel.edu>
2020-12-18 16:43:41 -0800

    Fail test to clearall vendors

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,32 @@ using System;
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class VendorTest
+  public class VendorTest : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor ("name", "description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void VendorProperties_AddPropertiesToVendor_Properties()
+    {
+      string name = "Jessies Cafe";
+      string description = "Local Coffee shop";
+      Vendor newVendor = new Vendor(name, description);
+      int id = newVendor.Id;
+      string resultName = newVendor.Name;
+      string resultDescription = newVendor.Description;
+      Assert.AreEqual(name, resultName);
+      Assert.AreEqual(description, resultDescription);
+      Assert.AreEqual(1, id);
     }
   }
 }
